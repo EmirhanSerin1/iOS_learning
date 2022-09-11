3b5c937d5d00089eb773b3961c035ada817f95a3
EmirhanSerin1 <emirhanserin.1@gmail.com>
2022-09-11 21:34:50 +0300

    Initial Commit

---
+++ iOS_learning/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS_learning
+//
+//  Created by Emirhan Serin on 11.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
