e5b6f107663483b54818d2b8df30b10da53730b8
EmirhanSerin1 <emirhanserin.1@gmail.com>
2022-11-26 13:29:22 +0300

    commit

--- iOS_learning/ViewController.swift
+++ iOS_learning/ViewController.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var labelBottom: UILabel!
+    @IBOutlet weak var imageDiamondView: UIImageView!
+
+   
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let button = UIButton(frame: CGRect(x: 50, y: 100, width: 100, height: 50))
+          button.backgroundColor = .green
+          button.setTitle("Test Button", for: .normal)
+          button.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
+
+          self.view.addSubview(button)
     }
 
+    @objc func buttonAction(sender: UIButton!) {
+      print("Button tapped")
+    }
 
 }
 
